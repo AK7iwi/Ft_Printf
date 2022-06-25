f5def86314a81c37f8b254412d5da0fc531f3545
MikiLeKiwi <michael.feldman.ca@gmail.com>
2022-06-26 01:03:43 +0200

    function first_param

--- ft_printf.c
+++ ft_printf.c
@@ -6,33 +6,36 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 15:58:55 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/06/25 20:29:25 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/06/26 01:02:30 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
-# include <stdio.h>
+#include "ft_printf.h"
 
+
+int first_param(const char *str1, const char *str2)
+{
+    int i = ft_strlen(str1);
+
+    while(i > 0)
+    {
+        if (str1[1 + ft_strlen(str1) - i] == '%c') /* +2 */
+            write(1, str2, 1);
+        if (str1[1 + ft_strlen(str1) - i] == '%s')
+            write(1, str2, ft_strlen(str2));
+        if (str1[1 + ft_strlen(str1) - i] == '%%')
+            write(1, "%", 1);
+        i -= 2;
+    }
+}
 
 
 int ft_printf(const char *str, ...)
 {
     va_list ap;
     va_start(ap,str);
-    const char *val = va_arg(ap, const char *);
-    
-    if (str = '%c')
-        write(1, val, 1);
-    if (str == '%s')
-        write(1, val, strlen(val));
-    if (str == '%%')
-        write(1, "%", 1);
+    const char *val = va_arg(ap, const char *); /* malloc/free ici */
+    first_param(str,val);
     va_end(ap);
 }
 
-int main()
-{
-    ft_printf("%s", "oui");
-}
--- ft_printf.h
+++ ft_printf.h
@@ -6,18 +6,20 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 17:59:31 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/06/25 19:38:41 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/06/26 00:53:29 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINTF_H
 # define PRINTF_H
 
-
 # include <stdarg.h>
 # include <unistd.h>
+# include <string.h>
 # include <stdio.h>
 
-
+int ft_printf(const char *str, ...);
+int first_param(const char *str1,const char *str2);
+size_t	ft_strlen(const char *s);
 
 #endif
