383ae94243b4cb2de976b13e216c9359a91992a4
MikiLeKiwi <michael.feldman.ca@gmail.com>
2022-06-29 05:07:12 +0200

    ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 15:58:55 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/06/29 04:55:13 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/06/29 05:06:35 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,15 @@ int ft_printf(const char *str, ...)
     va_list ap;
     va_start(ap,str);
     
-    while (str[i] != '%')
-        write(1,str[i], 1);
-    if (str[i] == '%')
-        first_param((const char)str[i + 1],va_arg(ap,char *));
-    else 
+    while (str[i++] != '%')
+        write(1,&str[i], 1);
+    while (str[i++])
+    {
+        if (str[i] == '%')
+            first_param(str[i + 1],va_arg(ap,char *));
+        else 
             return(0);
-        i++;
+    }
     va_end(ap);
-    return(ft_strlen(str));
 }
 
-int main()
-{
-    ft_printf("%s", "oui");
-}
