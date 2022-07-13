3d5fa0a6ede0c8fc114b90185c3326072ee305f1
ASAPKiwi <michael.feldman.ca@gmail.com>
2022-07-13 22:59:22 +0200

    modif

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 17:59:31 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/07/13 20:00:03 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/07/13 20:19:23 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@
 int ft_printf(const char *str, ...);
 void first_param(const char str1,const char *str2);
 void	ft_puthexa(const char *s);
+
 
 #endif
