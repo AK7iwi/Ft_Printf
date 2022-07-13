d1404b5e89847e97dee7d0e9a7e2987efd679401
ASAPKiwi <michael.feldman.ca@gmail.com>
2022-07-13 20:15:19 +0200

    utils

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 17:59:31 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/07/13 19:54:34 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/07/13 20:00:03 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "./libft/libft.h"
 # include <stdarg.h>
+
 
 int ft_printf(const char *str, ...);
 void first_param(const char str1,const char *str2);
